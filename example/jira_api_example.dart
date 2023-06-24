6bd8f76e6ea5da50a6b4add73e57fe00ba62c099
Nikita Mishkov <mishkov.dd@gmail.com>
2023-06-24 08:55:10 +0300

    Implement prerry output

--- example/jira_api_example.dart
+++ example/jira_api_example.dart
@@ -1,4 +1,7 @@
+import 'dart:math' as math;
+
 import 'package:dotenv/dotenv.dart';
+import 'package:intl/intl.dart';
 import 'package:jira_api/jira_api.dart';
 
 Future<void> main() async {
@@ -22,4 +25,50 @@ Future<void> main() async {
       '${estimationGroup.groupStatus.name} => ${estimationGroup.estimation}',
     );
   }
+
+  final allStatus = searchResults.datedGroups.fold<Set<IssueStatus>>({},
+      (allStatuses, record) {
+    return allStatuses
+      ..addAll(record.groupedEstimations.fold<Set<IssueStatus>>({},
+          (innerStatuses, group) {
+        return innerStatuses..add(group.groupStatus);
+      }));
+  });
+
+  int maxLengthStatus = 0;
+  for (final status in allStatus) {
+    maxLengthStatus = math.max(maxLengthStatus, status.name.length);
+  }
+
+  const dateFormat = 'yyyy-MM-dd';
+  final DateFormat formatter = DateFormat(dateFormat);
+
+  final tableWidth = (allStatus.length + 4) +
+      dateFormat.length +
+      (maxLengthStatus + 2) * allStatus.length;
+  print('=' * tableWidth);
+  String statusesString = '';
+  for (final status in allStatus) {
+    statusesString += ' ${status.name.padRight(maxLengthStatus, " ")} |';
+  }
+  print('| $dateFormat |$statusesString');
+  print('=' * tableWidth);
+  for (final group in searchResults.datedGroups) {
+    String esmitationsString = '';
+    for (final status in allStatus) {
+      final estimatedGroup = group.groupedEstimations
+          .where((element) => element.groupStatus == status);
+
+      final String estimation;
+      if (estimatedGroup.isNotEmpty) {
+        estimation = estimatedGroup.single.estimation.toString();
+      } else {
+        estimation = '---';
+      }
+
+      esmitationsString += ' ${estimation.padRight(maxLengthStatus, " ")} |';
+    }
+    print('| ${formatter.format(group.date)} |$esmitationsString');
+  }
+  print('=' * tableWidth);
 }
